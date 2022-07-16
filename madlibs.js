f8ab92f24d74c544146a96831824f0d1642244f2
Amna-Amna <mailtoamna4@gmail.com>
2022-07-16 21:40:01 +0500

    first commit

---
+++ madlibs.js
@@ -0,0 +1,5 @@
+let clickBtn = document.querySelector(".click")
+let newPage = document.querySelector(".new-page")
+clickBtn.addEventListener("click",function(){
+    console.log("button is clicked")
+})
