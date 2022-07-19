f7e175d842f69db928b60f341a446b0ab9e220c4
Amna-Amna <mailtoamna4@gmail.com>
2022-07-19 19:30:19 +0500

    change in text style

--- madlibs.js
+++ madlibs.js
@@ -27,46 +27,25 @@ if (generateBtn) {
     generateBtn.addEventListener("click", function(){
         localStorage.setItem("number", number.value);
         localStorage.setItem("name", name.value);
-
+        localStorage.setItem("colour", colour.value);
+        localStorage.setItem("sport", sport.value);
+        localStorage.setItem("food", food.value);
+        localStorage.setItem("age", age.value);
+        localStorage.setItem("beverage", beverage.value);
+        localStorage.setItem("aim", aim.value);
+        localStorage.setItem("place", place.value);
     })
 }
 
 
 if (storySection) {
 
-    storySection.innerHTML = `<h3>Your Self</h3>
+    storySection.innerHTML = `<h3 class="self">Your Self</h3>
 
-    <p>I noticed a lot of times that on <span class="inserted text">${localStorage.getItem("number")}</span> date ,something always happen amaizing.
-    My name is <span class="inserted text">${localStorage.getItem("name")}</span> and my favourite colour is <span class="inserted text">${localStorage.getItem("colour")}</span>. 
-    I like to play <span class="inserted text">${localStorage.getItem("sport")}</span>.
-    i like to eat <span class="inserted text">${localStorage.getItem("food")}</span>.I am of <span class="inserted text">${localStorage.getItem("age")}</span> years old.
-    I want to shoot add drinking <span class="inserted text">${localStorage.getItem("beverage")}</span> on <span class="inserted text">${localStorage.getItem("place")}</span> during working as a <span class="inserted text">${localStorage.getItem("aim")}</span>.</p>                                                          
+    <p>I noticed a lot of times that on <span class="inserted-text">${localStorage.getItem("number")}</span> date ,something always happen amaizing.
+    My name is <span class="inserted-text">${localStorage.getItem("name")}</span> and my favourite colour is <span class="inserted-text">${localStorage.getItem("colour")}</span>. 
+    I like to play <span class="inserted-text">${localStorage.getItem("sport")}</span>.
+    i like to eat <span class="inserted-text">${localStorage.getItem("food")}</span>.I am of <span class="inserted-text">${localStorage.getItem("age")}</span> years old.
+    I want to shoot add drinking <span class="inserted-text">${localStorage.getItem("beverage")}</span> on <span class="inserted-text">${localStorage.getItem("place")}</span> during working as a <span class="inserted-text">${localStorage.getItem("aim")}</span>.</p>                                                          
      `
 }
-
-
-// const madsubmit = (event) =>{
-//     const form = new FormData(event.target)
-//     const userSubmission = Object.fromEntries(form)
-//     console.log(userSubmission)
-
-//     story =`
-// <h3>Your Self</h3>
-
-// <p>
-//   I noticed a lot of times that on
-//   <span class="inserted text">${userSubmission.number}</span> date
-//   ,something always happen amaizing. My name is
-//   <span class="inserted text">${userSubmission.name}</span> and my favourite
-//   colour is <span class="inserted text">${userSubmission.colour}</span>. I
-//   like to play <span class="inserted text">${userSubmission.sport}</span>. i
-//   like to eat <span class="inserted text">${userSubmission.food}</span>.I am
-//   of <span class="inserted text">${userSubmission.age}</span> years old. I
-//   want to shoot add drinking
-//   <span class="inserted text">${userSubmission.beverage}</span> on
-//   <span class="inserted text">${userSubmission.place}</span> during working
-//   as a <span class="inserted text">${userSubmission.aim}</span>.
-// </p>
-// `
-// storySection.innerHTML += story
-// }
